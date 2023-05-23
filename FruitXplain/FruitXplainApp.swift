3e29a5aaf26eaab18a605414a04b9e86f39772d8
Pawandeep Singh <pawandeepss@u.nus.edu>
2023-05-23 21:32:57 +0200

    Initial Commit

---
+++ FruitXplain/FruitXplainApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitXplainApp.swift
+//  FruitXplain
+//
+//  Created by Pawandeep Sekhon on 23/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitXplainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
